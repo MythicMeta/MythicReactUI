16311d47c4d6a00753498fe0396f24b9c8ba482f
its-a-feature <codybthomas@gmail.com>
2022-01-21 15:02:50 -0800

    bumping up the mythic ui version number

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import jwt_decode from 'jwt-decode';
 import {meState} from './cache';
 
 export const mythicVersion = "2.3.6";
-export const mythicUIVersion = "0.0.9";
+export const mythicUIVersion = "0.0.10";
 
 let fetchingNewToken = false;
 
