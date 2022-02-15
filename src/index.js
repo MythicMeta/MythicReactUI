39d020ba86d35f3f54bca5fa6b45a15d79ea7ab6
its-a-feature <codybthomas@gmail.com>
2022-02-14 16:56:13 -0800

    incrementing the UI version with the latest fixes

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import jwt_decode from 'jwt-decode';
 import {meState} from './cache';
 
 export const mythicVersion = "2.3.7";
-export const mythicUIVersion = "0.0.26";
+export const mythicUIVersion = "0.0.27";
 
 let fetchingNewToken = false;
 
